0ee3c449e8ba834e248f15d4563f8990554c8b32
liwn <mlla@zju.edu.cn>
2018-03-04 19:51:02 +0800

    add antweakbar

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <math.h>
+#include <AntTweakBar.h>
 
 using namespace std;
 using namespace Eigen;
@@ -10,8 +11,13 @@ using Vec3 = Vector3f;
 using Vec4 = Vector4f;
 
 float max0(float a) {
-  if(a > 0)return a;
+  if (a > 0)return a;
   return 0;
+}
+
+float max1(float a) {
+  if (a < 1)return a;
+  return 1;
 }
 
 class Buffer {
@@ -36,11 +42,82 @@ public:
 const int SCREEN_WIDTH  = 800;
 const int SCREEN_HEIGHT = 800;
 
-float para[10] = {1, 0, 0, 0, //ABCD
-                  1, 0, 0, //EFG
-                  1, 0, -1     //HIJ
+
+// Shapes scale
+float g_Zoom = 1.0f;
+// Shape orientation (stored as a quaternion)
+float g_Rotation[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+
+// Routine to set a quaternion from a rotation axis and angle
+// ( input axis = float[3] angle = float  output: quat = float[4] )
+void SetQuaternionFromAxisAngle(const float *axis, float angle, float *quat)
+{
+  float sina2, norm;
+  sina2 = (float)sin(0.5f * angle);
+  norm = (float)sqrt(axis[0] * axis[0] + axis[1] * axis[1] + axis[2] * axis[2]);
+  quat[0] = sina2 * axis[0] / norm;
+  quat[1] = sina2 * axis[1] / norm;
+  quat[2] = sina2 * axis[2] / norm;
+  quat[3] = (float)cos(0.5f * angle);
+}
+
+// Routine to convert a quaternion to a 4x4 matrix
+// ( input: quat = float[4]  output: mat = float[4*4] )
+void ConvertQuaternionToMatrix(const float *quat, float *mat)
+{
+  float yy2 = 2.0f * quat[1] * quat[1];
+  float xy2 = 2.0f * quat[0] * quat[1];
+  float xz2 = 2.0f * quat[0] * quat[2];
+  float yz2 = 2.0f * quat[1] * quat[2];
+  float zz2 = 2.0f * quat[2] * quat[2];
+  float wz2 = 2.0f * quat[3] * quat[2];
+  float wy2 = 2.0f * quat[3] * quat[1];
+  float wx2 = 2.0f * quat[3] * quat[0];
+  float xx2 = 2.0f * quat[0] * quat[0];
+  mat[0 * 4 + 0] = -yy2 - zz2 + 1.0f;
+  mat[0 * 4 + 1] = xy2 + wz2;
+  mat[0 * 4 + 2] = xz2 - wy2;
+  mat[0 * 4 + 3] = 0;
+  mat[1 * 4 + 0] = xy2 - wz2;
+  mat[1 * 4 + 1] = -xx2 - zz2 + 1.0f;
+  mat[1 * 4 + 2] = yz2 + wx2;
+  mat[1 * 4 + 3] = 0;
+  mat[2 * 4 + 0] = xz2 + wy2;
+  mat[2 * 4 + 1] = yz2 - wx2;
+  mat[2 * 4 + 2] = -xx2 - yy2 + 1.0f;
+  mat[2 * 4 + 3] = 0;
+  mat[3 * 4 + 0] = mat[3 * 4 + 1] = mat[3 * 4 + 2] = 0;
+  mat[3 * 4 + 3] = 1;
+}
+
+// Routine to multiply 2 quaternions (ie, compose rotations)
+// ( input q1 = float[4] q2 = float[4]  output: qout = float[4] )
+void MultiplyQuaternions(const float *q1, const float *q2, float *qout)
+{
+  float qr[4];
+  qr[0] = q1[3] * q2[0] + q1[0] * q2[3] + q1[1] * q2[2] - q1[2] * q2[1];
+  qr[1] = q1[3] * q2[1] + q1[1] * q2[3] + q1[2] * q2[0] - q1[0] * q2[2];
+  qr[2] = q1[3] * q2[2] + q1[2] * q2[3] + q1[0] * q2[1] - q1[1] * q2[0];
+  qr[3] = q1[3] * q2[3] - (q1[0] * q2[0] + q1[1] * q2[1] + q1[2] * q2[2]);
+  qout[0] = qr[0]; qout[1] = qr[1]; qout[2] = qr[2]; qout[3] = qr[3];
+}
+
+// Return elapsed time in milliseconds
+int GetTimeMs()
+{
+#if !defined(_WIN32)
+  return glutGet(GLUT_ELAPSED_TIME);
+#else
+  return (int)GetTickCount();
+#endif
+}
+
+
+float para[10] = {3, 0, 0, 0,  //ABCD
+                  4, 0, 0,     //EFG
+                  5, 0, -1     //HIJ
                  };
-Vec3 light(1, 1, 1);
+Vec3 light(0.57735f, 0.57735f, 0.57735f);
 
 Vec3 raycast(float x, float y) {
   Matrix4f Q;
@@ -58,29 +135,38 @@ Vec3 raycast(float x, float y) {
   float delta = b * b - 4 * a * c;
   if (delta < 0)
     return Vec3(0, 0, 0);
+  float z;
 
-  float z = (-b + sqrt(delta)) / (2 * a);
-  Vec3 n =  (Q * Vec4(x, y, z, 1)).normalized().head(3);
+  if (a == 0) {//highlight z=[-\inf, \inf]
+    if (fabs(c) < 0.005)
+      return Vec3(1, 1, 1);
+    else
+      return Vec3(0, 0, 0);
+  } else {
+    z = (-b + sqrt(delta)) / (2 * a);
+  }
 
-  light.normalize();
+  Vec3 n =  (Q * Vec4(x, y, z, 1)).head(3).normalized();
+
   float diffuse = max0(n.dot(light));
 
   Vec3 reflect = (2 * n.dot(light) * n - light);
-  cout << n << endl;
-  cout << n.dot(light) << endl;
-  cout << reflect.norm() <<endl;;
-  exit(0);
   Vec3 view(0, 0, 1);
 
   float ambient = 0.2;
-  float specular = pow(max0(view.dot(reflect)), 4) * 2;
+  float specular = pow(max0(view.dot(reflect)), 8) * 0.4;
 
-  cout << specular << endl;
+  float intensity = max1(ambient + 0.5 * diffuse);
+  Vec3 color = intensity * Vec3(1, 1, 0) + specular * Vec3(1, 1, 1);
+  for (size_t i = 0; i < 3; ++i) {
+    color[i] = max1(color[i]);
+  }
 
-  return (ambient + 0.5*diffuse) * Vec3(0, 0, 1) + specular * Vec3(1,1,1);
+  return color;
 }
 
 void draw_quadric(Buffer& buf) {
+  light.normalize();
   for (size_t i = 0; i < buf.w; ++i) {
     for (size_t j = 0; j < buf.h; ++j) {
       float cx = (i * 2.0 / buf.w - 1) * 1.1;
@@ -104,6 +190,9 @@ void render_scene(void) {
   draw_quadric(buf);
   glDrawPixels(w, h, GL_RGB, GL_FLOAT, buf.raw_data());
 
+  // Draw tweak bars
+  TwDraw();
+
   glutSwapBuffers();
   glutPostRedisplay();
 }
@@ -114,6 +203,12 @@ void change_size(int w, int h) {
   glViewport(0, 0, w, h);
   glOrtho(-1, 1, -1, 1, -2, 2);
   glMatrixMode(GL_MODELVIEW);
+  // Send the new window size to AntTweakBar
+  TwWindowSize(w, h);
+}
+
+void my_terminate(void) {
+  TwTerminate();
 }
 
 int main(int argc, char *argv[]) {
@@ -126,6 +221,46 @@ int main(int argc, char *argv[]) {
 
   glutDisplayFunc(render_scene);
   glutReshapeFunc(change_size);
+  atexit(my_terminate);
+
+  TwInit(TW_OPENGL, NULL);
+  // Set GLUT event callbacks
+  glutMouseFunc((GLUTmousebuttonfun)TwEventMouseButtonGLUT);
+  glutMotionFunc((GLUTmousemotionfun)TwEventMouseMotionGLUT);
+  glutPassiveMotionFunc((GLUTmousemotionfun)TwEventMouseMotionGLUT);
+  glutKeyboardFunc((GLUTkeyboardfun)TwEventKeyboardGLUT);
+  glutSpecialFunc((GLUTspecialfun)TwEventSpecialGLUT);
+  // - Send 'glutGetModifers' function pointer to AntTweakBar;
+  //   required because the GLUT key event functions do not report key modifiers states.
+  TwGLUTModifiersFunc(glutGetModifiers);
+
+  TwBar *bar = TwNewBar("TweakBar"); // Pointer to the tweak bar
+  TwDefine(" TweakBar size='180 360' color='96 216 224' "); // change default tweak bar size and color
+
+  // Add 'g_Zoom' to 'bar': this is a modifable (RW) variable of type TW_TYPE_FLOAT. Its key shortcuts are [z] and [Z].
+  TwAddVarRW(bar, "Zoom", TW_TYPE_FLOAT, &g_Zoom,
+             " min=0.01 max=2.5 step=0.01 keyIncr=z keyDecr=Z help='Scale the object (1=original size).' ");
+
+  // Add 'g_Rotation' to 'bar': this is a variable of type TW_TYPE_QUAT4F which defines the object's orientation
+  TwAddVarRW(bar, "Rotation", TW_TYPE_QUAT4F, &g_Rotation,
+             " label='Rotation' opened=true help='Change the object orientation.' ");
+
+  TwDefine(" GLOBAL help='This example shows Ax^2+2Bxy+2Cxz+2Dx+Ey^2+2Fyz+Gy+Hz^2+2Iz+J=0.' ");
+
+  TwAddVarRW(bar, "A", TW_TYPE_FLOAT, para,   " group='Equation' ");
+  TwAddVarRW(bar, "B", TW_TYPE_FLOAT, para+1, " group='Equation' ");
+  TwAddVarRW(bar, "C", TW_TYPE_FLOAT, para+2, " group='Equation' ");
+  TwAddVarRW(bar, "D", TW_TYPE_FLOAT, para+3, " group='Equation' ");
+  TwAddVarRW(bar, "E", TW_TYPE_FLOAT, para+4, " group='Equation' ");
+  TwAddVarRW(bar, "F", TW_TYPE_FLOAT, para+5, " group='Equation' ");
+  TwAddVarRW(bar, "G", TW_TYPE_FLOAT, para+6, " group='Equation' ");
+  TwAddVarRW(bar, "H", TW_TYPE_FLOAT, para+7, " group='Equation' ");
+  TwAddVarRW(bar, "I", TW_TYPE_FLOAT, para+8, " group='Equation' ");
+  TwAddVarRW(bar, "J", TW_TYPE_FLOAT, para+9, " group='Equation' ");
+
+  TwAddVarRW(bar, "Light", TW_TYPE_DIR3F, light.data(), 
+               " label='Inv light dir' help='Change the light direction.' ");
+
   glutMainLoop();
   return 0;
 }
